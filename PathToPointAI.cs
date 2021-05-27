e3ccbec314a263c14e23ea11f200f14a3a21b1a9
Alexandr Gurov <alex64644@gmail.com>
2021-05-27 13:21:17 +0300

    Init commit

---
+++ PathToPointAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+ using UnityEditor.Experimental.UIElements;
+ using UnityEngine;
+ using UnityEngine.AI;
+ using UnityEngine.Serialization;
+ //Класс точке предвижения, имеет 3 параметра: точку, минимальное и максимальное время нахождения на точке
+ [System.Serializable] public class MovePoint
+ {
+     public Transform point;
+     public float minAfk;
+     public float maxAfk;
+     //конструктор класса
+     public MovePoint(Transform point, float minAfk, float maxAfk)
+     {
+         this.point = point;
+         this.minAfk = minAfk;
+         this.maxAfk = maxAfk;
+     }
+ }
+ //Скрипт предназначем для объектов с компонентов Unit(Враждебные и дружелюбные НПС), отвечает за перемещение объекта по заданным точка
+ public class PathToPointAI : MonoBehaviour
+ {    
+     [FormerlySerializedAs("Loop")] public bool loop;//повторять передвижение в цикле?
+     
+     public bool moveInRandomOrder = false;//перемещаться по точкам в случайном порядке?
+ 
+     public bool afk = false;//нужно ли стоят на точке?
+     private int _pointIndex = 0;
+     private bool _inPath = false;
+     private Unit _unit;
+     private float _time;
+ 
+     public MovePoint[] point;//создаём массив типа MovePoint
+     
+     //подключаемся к компоненту Unit при инициализации объекта
+     private void Awake()
+     {
+         _unit = GetComponent<Unit>();
+     }
+     //проверяем в пути ли объект, если расстояние до конечной точке мало, возвращаем false
+     private bool InMove()
+     {
+         return !(_unit.navMeshAgent.remainingDistance < _unit.navMeshAgent.radius*1.50f);
+     }
+     //срабатывает каждый кадр игры
+     private void Update()
+     {
+         _time += Time.deltaTime; // овеличивает переменную с временем на разницу времени между кадрами
+         if (_unit._patrol && _time > 0.8f)//если объект должен патрулировать и прошло больше 0,8 секунды
+         {
+             //Debug.Log(_time);
+             _time = 0f;//сбрасываем счётчик времени
+             if (loop && (_pointIndex >= point.Length) && !_inPath)//если мы движемся в цикле и индекс точки вышел за пределы массива, то обнуляем его
+             {
+                 _pointIndex = 0;
+                 //Debug.Log("Index set zero");
+             }
+             else if (!_inPath && !InMove() && (_pointIndex < point.Length))//если мы не в функции пермещения, и не движемся, и индекс не вышел за пределы массива
+             {
+                 if (moveInRandomOrder)//если задано перемещение в случайном порядке, то генерирем индекс
+                     _pointIndex = Random.Range(0, point.Length - 1);
+                 //Debug.Log("DoSomething " + _pointIndex + " / " + point.Length);
+                 StartCoroutine(DoSomething());//запускаем параллельную функцию перемещения
+             }
+         }
+     }
+     //функция перемещения
+     private IEnumerator DoSomething()
+     {
+         _inPath = true;
+         if(afk)//если надо ждать на точке, то рандомим значение для ожидания от минимально до максимально заданного
+             yield return new WaitForSeconds(Random.Range(point[_pointIndex].minAfk, point[_pointIndex].maxAfk));
+         _unit.Move(point[_pointIndex].point.position);//отправляем объект на точку
+         yield return new WaitForSeconds(0.6f);//небольшая задержка
+         _pointIndex++;//увеличиваем индекс для массива точек
+         _inPath = false;
+     }
+ }
---
+++ runoutAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+//Скрипт предназначен для объектов не имеющих компонента Unit, например курицы, скрипт заставит объект отдалсять от игрока, если тот подошёл к нему слишком близко
+public class runoutAI : MonoBehaviour 
+{
+    private NavMeshAgent _agent;
+    public float EnemyDistanceRun = 4.0f;
+    private float _time = 0f;
+    private bool _inRoutine = false;
+	//инициализация, подключаемся к компоненту NavMeshAgent нашего объекта, на который помещём скрипт
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+	//выполняется каждый кадр игры, проверяем прошлоли 1,5 секунды с предыдёщей итерации последующих проверок,
+	//далее проверяем не выполняется ли сейчас паралленый процесс отдаления от игрока и проверяем не подошёл ли игрок слишком близко
+    private void Update()
+    {
+        if (_time >= 1.5f)
+        {
+            if (!_inRoutine && CheckPlayer())
+            {
+                StartCoroutine(DoSomething());
+            }
+            _time = 0f;
+        }
+        else
+        {
+            _time += Time.deltaTime;
+        }
+    }
+    //вычисляем расстояние между игроком и объектом, если расстояние меньше допустимого возращаем true
+    private bool CheckPlayer()
+    {
+        var distance = Vector3.Distance(_agent.transform.position, playerManager.instance.player.transform.position);
+        //Debug.Log("Distance: " + distance);
+        return distance < EnemyDistanceRun;
+    }
+	//функция выполняемая в параллельном потоке, спомощью координат место положения игрока и объекта вычисляет вектор противоположнывй вектору между игроком и объектом
+	//далее отправляем объект в точку на полученном векторе и делаем задержку 1,2 секунды, чтобы функция не была вызвана повторна
+    private IEnumerator DoSomething()
+    {
+        _inRoutine = true;
+        var position = _agent.transform.position;
+        var dirToPlayer = position - playerManager.instance.player.transform.position;
+        var newPos = position + dirToPlayer;
+        _agent.SetDestination(newPos);
+        yield return new WaitForSeconds(1.2f);
+        _inRoutine = false;
+    }
+}
